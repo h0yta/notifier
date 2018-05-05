23c0acaeb786580f9630f2662a4dacce9bdd4c42
h0yta <oscar@vettig.se>
2018-05-05 17:42:30 +0200

    Enhanced status

--- app/bookNotifier.js
+++ app/bookNotifier.js
@@ -7,67 +7,94 @@ var fs = require('fs');
 
 let properties = require('./properties.json');
 
-var run = function () {
-  getBooksFile().then((bookList) => {
-    bookList.forEach((book) => {
-      getLatestBook(book).then(function (latestBook) {
-        if (latestBook.title !== '') {
-          if (book.latestBookTitle === null || book.latestBookTitle === undefined || book.latestBookTitle === '') {
-            book.latestBookTitle = latestBook.title;
-            book.latestBookStatus = latestBook.status;
-            console.log(" Saknar bok för " + book.author + " sparar senaste -> " + latestBook.title);
-          } else if (book.latestBookTitle !== latestBook.title) {
-            book.latestBookTitle = latestBook.title;
-            book.latestBookStatus = latestBook.status;
-            slack.send("Boktips - ny bok '" + latestBook.title + "' av " + book.author + " (" + latestBook.status + ")");
-            console.log(" Boktips - ny bok '" + latestBook.title + "' av " + book.author + " (" + latestBook.status + ")");
-          } else if (book.latestBookStatus !== latestBook.status) {
-            book.latestBookTitle = latestBook.title;
-            book.latestBookStatus = latestBook.status;
-            slack.send("Boktips - ny status för boken '" + latestBook.title + "' av " + book.author + " (" + latestBook.status + ")");
-            console.log(" Boktips - ny status för boken '" + latestBook.title + "' av " + book.author + " (" + latestBook.status + ")");
-          } else {
-            console.log(" Inga nyheter för " + book.author);
-          }
+var run = async function () {
+  iconv.skipDecodeWarning = true;
 
-          fs.writeFileSync(__dirname + '/books.json', JSON.stringify(bookList, null, 2));
-        }
-      }).catch((error) => {
-        slack.send("Help me I crashed trying to find the latest book.");
-        console.error(error);
-      });
+  try {
+    let bookList = await getBooksFile();
+    bookList.forEach(async (book) => {
+      let latestBook = await getLatestBookAdlibris(book);
+      if (book.latestBookTitle === null || book.latestBookTitle === undefined || book.latestBookTitle === '') {
+        book.latestBookTitle = latestBook.title;
+        book.latestBookStatus = latestBook.status;
+        console.log(" Saknar bok för " + book.author + " sparar senaste -> " + latestBook.title);
+      } else if (book.latestBookTitle !== latestBook.title) {
+        book.latestBookTitle = latestBook.title;
+        book.latestBookStatus = latestBook.status;
+        slack.send("Boktips - ny bok '" + latestBook.title + "' av " + book.author + " (" + latestBook.status + ")");
+        console.log(" Boktips - ny bok '" + latestBook.title + "' av " + book.author + " (" + latestBook.status + ")");
+      } else if (book.latestBookStatus !== latestBook.status) {
+        book.latestBookTitle = latestBook.title;
+        book.latestBookStatus = latestBook.status;
+        slack.send("Boktips - ny status för boken '" + latestBook.title + "' av " + book.author + " (" + latestBook.status + ")");
+        console.log(" Boktips - ny status för boken '" + latestBook.title + "' av " + book.author + " (" + latestBook.status + ")");
+      } else {
+        console.log(" Inga nyheter för " + book.author);
+      }
+
+      fs.writeFileSync(__dirname + '/books.json', JSON.stringify(bookList, null, 2));
     });
-  }).catch((error) => {
-    slack.send("Help me I crashed trying to read the book file.");
-    console.error(error);
-  });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-var getBooksFile = function () {
-  return new Promise(function (res, rej) {
-    fs.readFile(__dirname + '/books.json', 'utf8', function (err, data) {
-      try {
-        res(JSON.parse(data));
-      } catch (error) {
-        rej(error);
+var getBooksFile = async function () {
+  let data = fs.readFileSync(__dirname + '/books.json', 'utf8');
+  return JSON.parse(data);
+}
+
+var getLatestBookBokus = async function (book) {
+  return new Promise(function (resolve, reject) {
+    var url = properties.bokusUrl.replace("#####", book.author);
+    request(url, function (err, response, body) {
+      if (err) {
+        console.err(" Something went wrong, couldn't parse parseBookInfo.")
+      } else {
+        var $ = cheerio.load(iconv.decode(body, 'iso-8859-1'));
+        let first = $('.ProductList__item')
+          .children()
+          .first();
+
+        let title = first.find($('.Item__title--large'))
+          .text()
+          .trim();
+        let status = first.find($('.ProductList__status'))
+          .text()
+          .trim();
+        let book = {
+          'title': title,
+          'status': translateStatus(status)
+        }
+
+        resolve(book);
       }
     });
   });
 }
 
-var getLatestBook = function (book) {
+var getLatestBookAdlibris = function (book) {
   return new Promise(function (resolve, reject) {
-    var url = properties.bokusUrl.replace("#####", book.author);
-    request({ 'url': url, encoding: null }, function (err, response, body) {
+    var url = properties.adlibrisUrl.replace("#####", book.author);
+    request(url, function (err, response, body) {
       if (err) {
-        console.err(" Something went wrong, couldn't parse parseBookInfo.");
-        rej(err);
+        console.err(" Something went wrong, couldn't parse parseBookInfo.")
       } else {
-        var $ = cheerio.load(iconv.decode(body, 'iso-8859-1'));
-        let first = $('.ProductList__item').children().first();
+        var $ = cheerio.load(body);
+        var books = [];
+        let first = $('.purchase-and-processing')
+          .children()
+          .first();
 
-        let title = first.find($('.Item__title--large')).text().trim();
-        let status = first.find($('.ProductList__status')).text().trim();
+        let title = first.find($('.heading--searchlist-title')).text().trim();
+        let status = first.find($('span.processing-time'))
+          .clone()
+          .children()
+          .remove()
+          .end()
+          .text()
+          .trim();
+
         let book = {
           'title': title,
           'status': translateStatus(status)
@@ -82,6 +109,8 @@ var getLatestBook = function (book) {
 const translateStatus = (status) => {
   if (status === 'Ännu ej utkommen') {
     return 'Kommande';
+  } else if (status.indexOf('Förväntas skickas under') === 0) {
+    return 'Förhandsboka';
   } else {
     return 'I lager';
   }
--- app/slack.js
+++ app/slack.js
@@ -2,8 +2,10 @@ var Promise = require('promise');
 var Slack = require('slack-node');
 var fs = require('fs');
 
-var sendNotification = function(message) {
-  return new Promise(function(res, rej) {
+let properties = require('./properties.json');
+
+var sendNotification = function (message) {
+  return new Promise(function (res, rej) {
     getPropertiesFile().then((property) => {
       let properties = JSON.parse(property);
       sendSlackNotification(properties.apiToken, message);
@@ -13,21 +15,26 @@ var sendNotification = function(message) {
   });
 }
 
-var getPropertiesFile = function() {
-  return new Promise(function(res, rej) {
-    fs.readFile(__dirname+'/properties.json', 'utf8', function(err, data) {
+var getPropertiesFile = function () {
+  return new Promise(function (res, rej) {
+    fs.readFile(__dirname + '/properties.json', 'utf8', function (err, data) {
       res(data);
     });
   });
 }
 
-var sendSlackNotification = function(apiToken, message) {
+var sendSlackNotification = function (apiToken, message) {
+  if (properties.skipSlack) {
+    console.log('Slack was turned off!');
+    return;
+  }
+  return;
   var Slack = require('slack-node');
   slack = new Slack(apiToken);
   slack.api('chat.postMessage', {
-    text:message,
-    channel:'#notifications'
-  }, function(err, response){
+    text: message,
+    channel: '#notifications'
+  }, function (err, response) {
     if (err) {
       console.log('Error sending slack message: ', response);
     }
