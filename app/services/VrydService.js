2b07c459b03191fb00e876f790df2e7b42218dc1
h0yta <oscar@vettig.se>
2020-07-13 23:08:56 +0200

    VrydService could not search for author and title concatenated

--- app/services/VrydService.js
+++ app/services/VrydService.js
@@ -5,7 +5,9 @@ const stringSimilarity = require('string-similarity');
 let properties = require('../../resources/properties.json');
 
 const getLibraryBook = async (author, book) => {
-  let url = properties.vrydLibraryUrl.replace("#####", util.concatAuthorAndBook(author, book));
+  let url = properties.vrydLibraryUrl.replace("#####", book);
+
+  console.log('url -> ', url);
 
   return puppeteer
     .launch()
@@ -17,7 +19,7 @@ const getLibraryBook = async (author, book) => {
       });
     }).then((html) => {
       let $ = cheerio.load(html);
-      let result = $('.product-list-item-link')
+      let resultTitle = $('.product-list-item-link')
         .children()
         .first()
         .text()
@@ -26,13 +28,19 @@ const getLibraryBook = async (author, book) => {
         .replace(/:.*/gi, '')
         .trim();
 
-      let link = $('.product-list-item-link')
+      let resultAuthor = $('.product-list-author')
+        .children()
+        .text()
+        .replace(/[\s]{2,}/gi, '');
+      resultAuthor = resultAuthor.split('‚');
+
+      let resultLink = $('.product-list-item-link')
         .first()
         .attr('href');
 
       let status = 'EJ_TIILGANGLIG_FOR_LAN';
       let store = 'Vaggeryds bibliotek';
-      if (result !== null && stringSimilarity.compareTwoStrings(result, book) >= 0.8) {
+      if (authorMatches(resultAuthor, author) && titleMatches(resultTitle, book)) {
         status = 'TILLGANGLIG_FOR_LAN';
       }
 
@@ -40,7 +48,7 @@ const getLibraryBook = async (author, book) => {
         'title': book,
         'status': status,
         'store': store,
-        'link': util.createBookUrl(url, link)
+        'link': util.createBookUrl(url, resultLink)
       }
 
       return libBook;
@@ -48,6 +56,16 @@ const getLibraryBook = async (author, book) => {
       console.log(' Error in getLibraryBook in VrydService', err);
     });
 
+}
+
+const authorMatches = (resultAuthors, author) => {
+  return resultAuthors.filter(a => {
+    return stringSimilarity.compareTwoStrings(a, author) >= 0.8;
+  }).length > 0;
+}
+
+const titleMatches = (resultTitle, book) => {
+  return resultTitle !== null && stringSimilarity.compareTwoStrings(resultTitle, book) >= 0.8;
 }
 
 module.exports.getLibraryBook = getLibraryBook;
